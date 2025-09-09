ea9afa7fd957bb4292032f577804b34d9853cc34
ravi2krishna <ravi2krishna@gmail.com>
2025-09-09 15:42:48 +0530

    Syntax

---
+++ demo.py
@@ -0,0 +1,22 @@
+# means comment 
+
+# we are using script mode 
+
+# Syntax (Rules)
+print(9) # 9 is numeric 
+
+# good morning is text data
+# Syntax / Rule : If you have text data we need this data to enclosed in "" or ''
+# print(good morning) # SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print("good morning")
+print('good morning')
+
+# print(hello) # NameError: name 'hello' is not defined. Did you mean: 'help'?
+hello = "hello hi"
+print(hello)
+
+# class = "10th Standard" # SyntaxError: invalid syntax
+# print(class) 
+
+classes = "10th Standard" # SyntaxError: invalid syntax
+print(classes) 
