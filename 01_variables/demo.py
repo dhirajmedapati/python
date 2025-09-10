7397e624ceed38b90e23553c4d2cb8aedfd1c73b
ravi2krishna <ravi2krishna@gmail.com>
2025-09-10 16:19:51 +0530

    variables

--- 01_variables/demo.py
+++ 01_variables/demo.py
@@ -48,3 +48,49 @@ print(id(list_nums_two))
 
 print(id(list_nums_one[0]))
 print(id(list_nums_two[0]))
+
+# variables
+x = "python "
+y = "is "
+z = "easy"
+
+# o/p - python is easy
+# print(xyz) # NameError: name 'xyz' is not defined
+print(x+y+z) # Concatenation : Joining Strings
+
+x = 10
+y = 20
+z = 30
+print(x+y+z) 
+
+# req : combine multiple types of data
+name = "Ravi"
+age = 20
+# o/p - My name is Ravi and my age is 20
+# print("My name is "+name+ " and my age is "+age) # TypeError: can only concatenate str (not "int") to str
+# print("My name is ",name +" and my age is ",age, +" and after 5 years my age is ", age+5) # TypeError: bad operand type for unary +: 'str'
+print(f"My name is {name} and my age is {age}") # Interpolation new style python
+print(f"My name is {name} and my age is {age} and after 5 years my age is  {age+5}") # Interpolation new style python
+
+
+x,y,z = 10,20,30
+print(x)
+print(y)
+print(z)
+
+# LHS = RHS
+# x,y,z = 10,20,30,40 # ValueError: too many values to unpack (expected 3)
+print(x)
+print(y)
+print(z)
+
+# multiple values assigned
+a = 10
+b = 20
+c = 10
+d = 10
+
+a = c = d = 10
+print(a)
+print(c)
+print(d)
