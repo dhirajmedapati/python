e8eb37d31f9a6be8d0523015cc76748391d83c2c
ravi2krishna <ravi2krishna@gmail.com>
2025-09-15 15:31:12 +0530

    Jumping / Branching Statements

---
+++ 04_control_statements/branch.py
@@ -0,0 +1,17 @@
+# Jumping / Branching Statements 
+
+# break exit loop
+for num in range(10):    
+    if num == 5:
+        break
+    print(num)
+
+# continue skip loop
+for num in range(10):    
+    if num == 5:
+        continue
+    print(num)
+
+# pass
+if True:
+    pass
