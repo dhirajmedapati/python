1c44f0b14722b7ad947a421239f202cde6c6a09f
ravi2krishna <ravi2krishna@gmail.com>
2025-09-15 15:31:07 +0530

    nested loops

--- 04_control_statements/nested.py
+++ 04_control_statements/nested.py
@@ -10,3 +10,21 @@ if age >=18:
         print("you need an ID to vote ")
 else:
     print("you are too young to vote ")
+    
+# Nested Loops - Math Table 
+for outer in range(1,4):
+    for inner in range(1,4):
+        print(f"{outer} X {inner} = {outer * inner}")
+    print("-----------")
+
+outer = 1
+while outer < 4:
+    inner = 1
+    while inner < 4:
+        print(f"{outer} X {inner} = {outer * inner}")
+        inner+=1
+    print("-----------")
+    outer+=1
+        
+    
+    
