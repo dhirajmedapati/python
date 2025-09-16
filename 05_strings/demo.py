1c16315ef621bf1f4e663332dfcc106c46ba1351
ravi2krishna <ravi2krishna@gmail.com>
2025-09-16 16:02:11 +0530

    16th Sep - String methods

--- 05_strings/demo.py
+++ 05_strings/demo.py
@@ -54,4 +54,81 @@ print(text[-4])
 # length of string
 print(len(text))
 
-# Slicing 
+# range(start, stop, step)
+
+# Slicing : extract portion of string
+# slice[start:stop:step]
+text = "python"
+print(text[:]) # python
+print(text[::]) # python
+print(text[1:3]) # yt
+print(text[2:]) # ython
+print(text[:4]) # pyth
+print(text[0:3:1]) # pyt  start:stop:step
+print(text[0:5:2]) # pto
+
+    #  0  1  2   3  4  5 (Positive Index)
+	#  p  y  t   h  o  n
+	# -6 -5  -4 -3  -2 -1 (Negative Index)
+
+print(text[-4:-1]) # tho
+print(text[-4:-1:1]) # tho
+print(text[-4:-1:-1]) # empty
+print(text[-4:-6:-1]) # ty
+
+# string repetition
+laugh = "HaHa"
+print(laugh)
+hard_laugh = "HaHaHaHaHaHaHaHaHaHaHaHaHaHaHaHa"
+print(hard_laugh)
+
+hard_laugh = laugh * 10
+print(hard_laugh)
+
+# String Concatenation
+first_name = "Ravi"
+last_name = "Krishna"
+full_name = first_name + last_name
+print(full_name)
+
+# String Methods 
+
+# simulate gmail like functionality
+email = input("Enter Your Email ID: ")
+format_email = email.lower() + "@gmail.com"
+print("User Given ID: "+email)
+print("Gmail Auto Format ID: "+format_email)
+
+# simulate PAN functionality
+pan = input("Enter Your PAN ID: ")
+
+if pan.isalnum():
+    format_pan = pan.upper()
+    print("User Given PAN: "+pan)
+    print("Formatted PAN: "+format_pan)
+else:
+    print("User Given PAN is Invalid: "+pan)
+    
+# validate email : ravi@gmail.com --> ravigmail.com
+email = input("Enter Your Email ID: ")
+if email.find("@") != -1:
+    print("valid email")
+else:
+    print("invalid email")
+    
+if "@" in email:
+    print("valid email")
+else:
+    print("invalid email")
+    
+# redirect operator based on country code
+phone_number = input("Enter Phone Number with IS Code: ")
+if phone_number.startswith("+1"):
+    print("Call Connected To USA")
+elif phone_number.startswith("+91"):
+    print("Call Connected To India")
+else:
+    print("ISD available only to USA & India")
+    
+# check if email sync is possible
+
